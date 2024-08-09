8182e4d5eb6b92f62efb11fde1c2bec64f50ab8c
Lincoln Stein <lstein@gmail.com>
2024-08-09 18:43:55 -0400

    remove "device" configuration

--- invokeai/backend/util/devices.py
+++ invokeai/backend/util/devices.py
@@ -2,6 +2,7 @@
 
 from typing import TYPE_CHECKING, Dict, Literal, Optional, Set, Union
 
+import threading
 import torch
 from deprecated import deprecated
 
@@ -70,9 +71,9 @@ class TorchDevice:
     @classmethod
     def _choose_device(cls) -> torch.device:
         app_config = get_config()
-        if app_config.device != "auto":
-            device = torch.device(app_config.device)
-        elif torch.cuda.is_available():
+        if app_config.devices:
+            return torch.device(app_config.devices[0])
+        if torch.cuda.is_available():
             device = CUDA_DEVICE
         elif torch.backends.mps.is_available():
             device = MPS_DEVICE
@@ -92,7 +93,7 @@ class TorchDevice:
     def choose_torch_dtype(cls, device: Optional[torch.device] = None) -> torch.dtype:
         """Return the precision to use for accelerated inference."""
         config = get_config()
-        device = device or cls._choose_device()
+        device = device or cls.choose_torch_device()
         if device.type == "cuda" and torch.cuda.is_available():
             device_name = torch.cuda.get_device_name(device)
             if "GeForce GTX 1660" in device_name or "GeForce GTX 1650" in device_name:
